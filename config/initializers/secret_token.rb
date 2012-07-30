2dc123a6926504f167d5cf273507848e80552371
ajbraus <ajbraus@gmail.com>
2012-07-30 17:35:15 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kezz::Application.config.secret_token = '9ac18bf2c2ad5e1cc84ff0752ed8de30ef301f862d71092f721fd49e5fc8c936efa985368640272fd9da6f04995bfae604e52189aa96a7ca089f899d00ef2122'
