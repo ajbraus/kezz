5595055ba00fbd8f10a3fc4d8e9e064e3f4f2b26
Adam Braus <ajbraus@gmail.com>
2014-05-28 22:44:06 -0500

    blah'

--- app/assets/javascripts/poems.js
+++ app/assets/javascripts/poems.js
@@ -141,7 +141,9 @@ function countWordSyllables(word) {
   if(word.length <= 3) { return 1; }
   word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
   word = word.replace(/^y/, '');
-  return word.match(/[aeiouy]{1,2}/g).length;
+  if (word.match(/[aeiouy]{1,2}/g).length) {
+    return word.match(/[aeiouy]{1,2}/g).length;  
+  }
 }
 
 function lineSyllablesCount(line) {
