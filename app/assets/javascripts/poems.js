ae8891bfb76d7bbda529bb0b83e7f601884ec6c0
Adam Braus <ajbraus@gmail.com>
2014-05-28 21:55:26 -0500

    fixing kezzes

--- app/assets/javascripts/poems.js
+++ app/assets/javascripts/poems.js
@@ -1,31 +1,52 @@
-// STRATEGY 1
-function countWordSyllables(word) {
-  word = word.toLowerCase();
-  if(word.length <= 3) { return 1; }
-  word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
-  word = word.replace(/^y/, '');
-  return word.match(/[aeiouy]{1,2}/g).length;
-}
+// Strategy 0
+// textarea
+// each time you keyup take the whole thing
+// hack apart lines with "\n"
+// wrap each line in divs
+// count sylables
+// color the divs
+// put the whole thing to the right side
 
-function countLineSyllables(line, index) {
-  var line = line.split(/\s+/);
-  var totalSyllables = 0
-  line.forEach(function(word) { //count sylables per word
-    if (word.length > 0) {
-      totalSyllables += countWordSyllables(word) // add up syllables to count per line  
-    }
+// TODO check if any words are true or slant rhymes with any others
+// TODO tab = add four spaces --> save this and display it
+
+$(function() {
+  $('#poem').focus();
+
+  $('#title').on('keyup', function(e) {
+    var poemTitle = $('#title').val();
+    $('#poemTitle').text(poemTitle);
   });
-  if (totalSyllables == 5) { //style lines with 5 or 7 syllables
-    $('#poem div:nth-child(' + index + ')').css('color', 'blue');
-  } else if (totalSyllables == 7) {
-    $('#poem div:nth-child(' + index + ')').css('color', 'green');
-  } else if (totalSyllables == 10) {
-    $('#poem div:nth-child(' + index + ')').css('color', 'gold');
-  } else {
-    $('#poem div:nth-child(' + index + ')').css('color', 'grey');
-  }
-}
 
+  $('#poem').on('keyup', function(e) {
+    var poemText = $('#poem').val().replace( /\n/g, '<br \\>' ); // get poem
+    var lineArray = poemText.split("<br \\>"); // break into array of lines
+    //console.log(lineArray)
+    var formattedArray = []
+    lineArray.forEach(function(line, index) { 
+      var totalSyllables = lineSyllablesCount(line); // count syllables
+      //line = jQuery.parseHTML("<div>" + line + "</div>")
+      //console.log(line)
+      if (totalSyllables == 5) { //style lines with 5 or 7 syllables
+        line = "<span style='color:blue;'>" + line + "</span><br>"
+      } else if (totalSyllables == 7) {
+        line = "<span style='color:green;'>" + line + "</span><br>"
+      } else if (totalSyllables == 10) {
+        line = "<span style='color:gold;'>" + line + "</span><br>"
+      } else {
+        line = "<span style='color:grey;'>" + line + "</span><br>"
+      }
+      //line = index + 1 + " " + line // add line numbers
+      formattedArray.push(line);
+    }); 
+    var poemFormatted = formattedArray.join(" ");
+    // console.log(lineArray.join(" "))
+    $('#poemText').html(poemFormatted); //output formatted poem
+  });
+});
+
+
+// STRATEGY 1
 // wrap <div></div> around first line
 // poem is array of lines, first el is title (don't color it, add another <br> after it)
 // add | character between <div>'s
@@ -40,36 +61,32 @@ function countLineSyllables(line, index) {
 // grouping and coloring elements
 
 
-$(function() {
-  $('#poem').on('keydown', function(e) {
-    if ($('#poem').children().length == 0) { 
-      if (e.keyCode == 13) { // ENTER
-        //CHROME wrap first line of contentedtible in <div>
-        $('#poem').wrapInner("<div></div>")
-        //TODO add <br> after title
-      }
-    } 
-    var poem_html = $('#poem').html()
-    console.log( $('#poem').html())
-    // TODO add line number on left of new line
-    // TODO stripe white space from end of current line
-    var new_poem_html = poem_html.replace(/<\/div>/gi, "</div>|");
-    var new_poem_html = new_poem_html.replace(/<(?:.|\n)*?>/gm, '');
+// $(function() {
+//   $('#poem').focus();
+//   $('#poem').on('keydown', function(e) {
+//     if ($('#poem').children().length == 1) { 
+//       if (e.keyCode == 13) { // ENTER
+//         $('#poem').wrapInner("<div></div>") //CHROME wrap first line of contentedtible in <div>
+//         setTimeout(function(){$('#poem').children().first().append("<br>")}, 1); //add <br> after title
+//       }
+//     } 
+//     var poem_html = $('#poem').html()
+//     console.log( $('#poem').html())
+//     // TODO add line number on left of new line
+//     // TODO stripe white space from end of current line
+//     var new_poem_html = poem_html.replace(/<\/div>/gi, "</div>|");
+//     var new_poem_html = new_poem_html.replace(/<(?:.|\n)*?>/gm, '');
 
-    //console.log(poem_html);
-    //console.log(new_poem_html)
-    var linesArray = new_poem_html.split("|");
-    console.log(linesArray)
-    linesArray.forEach(function(line, index) { // break up lines by word
-      countLineSyllables(line, index + 1);
-    });
-  });
+//     //console.log(poem_html);
+//     //console.log(new_poem_html);
+//     var linesArray = new_poem_html.split("|")
+//     linesArray.shift();
+//     console.log(linesArray);
+//     linesArray.forEach(function(line, index) { // break up lines by
+//       countLineIndexSyllables(line, index + 1);
+//     });
+//   });
 
-  // TODO optional title
-  // TODO ignore punctuation in syllable count
-  // TODO check if any words are true or slant rhymes with any others
-  // TODO tab = add four spaces --> save this and display it
-  // TOOD hack off title, save title and body separately
 
 
 // ALTERNATIVE STRATEGY
@@ -116,7 +133,8 @@ $(function() {
 //     });
 //   });
 
-});
+// });
+
 
 function countWordSyllables(word) {
   word = word.toLowerCase();
@@ -126,7 +144,18 @@ function countWordSyllables(word) {
   return word.match(/[aeiouy]{1,2}/g).length;
 }
 
-function countLineSyllables(line, index) {
+function lineSyllablesCount(line) {
+  var line = line.split(/\s+/);
+  var totalSyllables = 0
+  line.forEach(function(word) { //count sylables per word
+    if (word.length > 0) {
+      totalSyllables += countWordSyllables(word) // add up syllables to count per line  
+    }
+  });
+  return totalSyllables
+}
+
+function countLineIndexSyllables(line, index) {
   var line = line.split(/\s+/);
   var totalSyllables = 0
   line.forEach(function(word) { //count sylables per word
@@ -135,12 +164,11 @@ function countLineSyllables(line, index) {
     }
   });
   if (totalSyllables == 5) { //style lines with 5 or 7 syllables
+    console.log(index)
     $('#poem div:nth-child(' + index + ')').css('color', 'blue');
   } else if (totalSyllables == 7) {
     $('#poem div:nth-child(' + index + ')').css('color', 'green');
   } else if (totalSyllables == 10) {
     $('#poem div:nth-child(' + index + ')').css('color', 'gold');
-  } else {
-    $('#poem div:nth-child(' + index + ')').css('color', 'grey');
-  }
+  } 
 }
